868cfaa8af9e48f45d029fa760b0588b94eaa28d
roca <RomelCampbell@gmail.com>
2014-01-20 14:00:14 -0500

    Made it to level 5

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function() {
-    this.resource('product', { path: '/:title' });
+    this.resource('product', { path: '/:product_id' });
   });
-  this.resource('contacts',function () {
-    	this.resource('contact',{path: '/:name'});
-    });
-  
+  this.resource('contacts', function() {
+    this.resource('contact', { path: '/:contact_id' });
+  });
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -29,52 +28,83 @@ App.ContactsIndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
-App.ProductRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.PRODUCTS.findBy('title', params.title); 
-  }
-});
-
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.CONTACTS;
-  }
-});
-App.ContactRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.CONTACTS.findBy('name', params.name);
+    return this.store.findAll('contact');
   }
 });
 
-App.PRODUCTS = [
-  {
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', { async: true }),
+  crafter: DS.belongsTo('contact',{async: true})
+});
+
+App.Product.FIXTURES = [
+ {  id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
-    image: 'images/products/flint.png'
+    image: 'images/products/flint.png',
+    reviews: [100,101],
+    crafter: 200
   },
   {
+    id: 2,
     title: 'Kindling',
     price: 249,
     description: 'Easily combustible small sticks or twigs used for starting a fire.',
     isOnSale: false,
-    image: 'images/products/kindling.png'
+    image: 'images/products/kindling.png',
+    reviews: [],
+    crafter: 201
   }
 ];
 
-App.CONTACTS = [
+App.Contact = DS.Model.extend({
+  name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string'),
+  products: DS.hasMany('product',{async: true})
+});
+App.Contact.FIXTURES = [
   {
+    id: 200,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
-    avatar: 'images/contacts/giamia.png'
+    avatar: 'images/contacts/giamia.png',
+    products: [1]
   },
   {
+    id: 201,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
-    avatar: 'images/contacts/anostagia.png'
+    avatar: 'images/contacts/anostagia.png',
+    products: [2]
+  }
+];
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+App.Review.FIXTURES = [
+  {
+    id: 100, 
+    text: "Started a fire in no time!"
+  },
+  {
+    id: 101, 
+    text: "Not the brightest flame, but warm!"
   }
 ];
